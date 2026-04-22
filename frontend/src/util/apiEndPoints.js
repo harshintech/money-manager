b6b14f6aa55c65def0918968d73b67fa33a009c1
harsintech <harshgohil02006@gmail.com>
2026-04-22 22:21:41 +0530

    change backend url

--- frontend/src/util/apiEndPoints.js
+++ frontend/src/util/apiEndPoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://money-manager-w098.onrender.com/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "deoaf8qmx";
 
 export const API_ENDPOINTS = {
